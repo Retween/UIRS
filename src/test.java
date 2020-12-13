2e023a80b81e7981a2da025769c44fad08ff3a27
Ernest <ernestzulpukarov@gmail.com>
2020-12-14 05:57:32 +0700

    Added orbitElem()

--- src/test.java
+++ src/test.java
@@ -1,15 +1,26 @@
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class test {
-    public static void main(String[] args)throws Exception {
-        try ( BufferedWriter writer = new BufferedWriter(new FileWriter("result.txt"))) {
-            coordinats coordinats = new coordinats(384399000, 0.0549, 5.1, 0, 0, 0);
-            for (double i = 0; i <= 2 * Math.PI; i += 2 * Math.PI / 100) {
-                writer.write(String.valueOf(coordinats.coordinates(i)[0]) + " " + String.valueOf(coordinats.coordinates(i)[1]) + " " + String.valueOf(coordinats.coordinates(i)[2]) + "\n");
-            }
-        }
+
+  public static void main(String[] args) throws Exception {
+    coordinates coordinates = new coordinates(1000000, 0.016, 0.00005, 2, 2, 0);
+
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter("result.txt"))) {
+      for (double i = 0; i <= 2 * Math.PI; i += 2 * Math.PI / 100) {
+        double[] x = coordinates.coordinates(i);
+        writer.write(x[0] + " " + x[1] + " " + x[2] + "\n");
+
+        coordinates.orbitElemCalc(x);
+      }
     }
+
+    HashMap<String, Double> orbitElem = coordinates
+        .orbitElemCalc(coordinates.coordinates(1.5 * Math.PI));
+    for (Map.Entry<String, Double> entry : orbitElem.entrySet()) {
+      System.out.println(entry.getKey() + ": " + entry.getValue());
+    }
+  }
 }
